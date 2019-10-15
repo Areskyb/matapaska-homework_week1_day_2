2f3102c40a31ac0282035ac80199d16e9e370717
Aresky Berkane <areskyberkane@gmail.com>
2019-10-15 17:17:15 +0100

    corrected errors of writing and function declarations

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -10,11 +10,11 @@ def subtract (num1, num2)
   return num1 - num2
 end
 
-def test_multiply (num1, num2)
+def multiply (num1, num2)
   return num1 * num2
 end
 
-def test_divide(num1, num2)
+def divide(num1, num2)
   return num1/num2
 end
 
@@ -22,15 +22,15 @@ def length_of_string (string)
   return string.length()
 end
 
-def test_join_string (string_1, string_2)
+def join_string (string_1, string_2)
   return string_1 + string_2
 end
 
-def add_string_as_number (strin_1, string_2)
+def add_string_as_number (string_1, string_2)
   return string_1.to_i() + string_2.to_i()
 end
 
-def test_number_to_full_name__month_1 (num)
+def number_to_full_name__month_1 (num)
   case num
     when 1
       return "January"
